eec026364633893d5317e92a887e9d6ebc9f7f61
Hércules Lopes <69056516+herculeslopes@users.noreply.github.com>
2021-02-26 11:29:53 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,46 +1,58 @@
 import os
 from natsort import natsorted
 
-path = input('PATH: ')
-name = input('Name: ')
+while True:
+    path = input('PATH: ')
+    name = input('Name: ')
 
-os.chdir(path)
+    os.chdir(path)
 
-dir_files = os.listdir()
-my_files = []
+    dir_files = os.listdir()
+    my_files = []
 
-for file in dir_files:
-    if file.startswith(name):
-        my_files.append(file)
+    for file in dir_files:
+        if file.startswith(name):
+            my_files.append(file)
 
-print()
-for f in natsorted(my_files):
-    print(f)
+    print()
+    for f in natsorted(my_files):
+        print(f)
 
-print()
-print('O Programa a seguir modificará os arquivos acima')
-print(f'Preview: {name} 00.cbx')
-res = input('Deseja prosseguir? ').strip().upper()[0]
+    print()
+    print('O Programa a seguir modificará os arquivos acima')
+    print(f'Preview: {name} 00.cbx')
 
-if res == "S":
+    while True:
+        res = input('Deseja prosseguir? ').strip().upper()[0]
 
-    for index, cbx in enumerate(natsorted(my_files)):
-        print()
-        print(f'Index: {index} - File: {cbx}')
+        if res == "S":
 
-        extension = os.path.splitext(f)[1]
+            for index, cbx in enumerate(natsorted(my_files)):
+                print()
+                print(f'Index: {index} - File: {cbx}')
 
-        print(f'File Extension: {extension}')
-        os.rename(cbx, f'{name} {index + 1:0>2}{extension}')
-        print(f'Renamed To: {name} {index + 1:0>2}{extension}')
+                extension = os.path.splitext(f)[1]
 
-elif res == "N":
+                print(f'File Extension: {extension}')
+                os.rename(cbx, f'{name} {index + 1:0>2}{extension}')
+                print(f'Renamed To: {name} {index + 1:0>2}{extension}')
+        
+            break
 
-    print('Programa Finalizado.')
+        elif res == "N":
+            break
 
-else:
+        else:
 
-    print('Resposta inválida.')
-    print('Finalilzando programa.')
+            print('Resposta inválida.')
+            print('Finalilzando programa.')
 
+    loop = input('Deseja renomear outro diretório? ').strip().upper()[0]
 
+    if loop == "S":
+        continue
+
+    elif loop == "N":
+        print('Programa Finalizado.')
+        break
+    
